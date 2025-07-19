b744565224703f5d620431740f6fc75d3673aacf
ricardocisco <ricardopenga70@gmail.com>
2025-07-18 23:48:14 -0300

    feat: bug auth

--- src/app/(auth)/login/loginAction.ts
+++ src/app/(auth)/login/loginAction.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { signIn } from "@/auth";
-import AuthError from "next-auth";
+import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function LoginAction(_prevState: any, formData: FormData) {
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,20 +10,10 @@ import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-zinc-900">
-      <Card className="max-w-sm w-full rounded-2xl mt-12 bg-zinc-800 border-none">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl text-amber-400">
-            Entrar
-          </CardTitle>
-          <CardDescription className="text-center text-white">
-            Crie uma conta gratuitamente
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <LoginForm />
-        </CardContent>
-      </Card>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
   );
 }
